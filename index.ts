d49244ee40c8a93572d6fd6eb12b27cfa5e0cc61
lucasmetron <lucasmetron@gmail.com>
2021-08-31 14:30:18 -0300

    tipando dados

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
-function criarMensagem(nome: string) {
-    return `Olá! ${nome}`
-}
+let meuBooleando: boolean = true;
+let meuNumero: number = 50.5 //para numeros, nao possui diferença entre int e float na tipagem, tudo é number
+let meuTexto: string = 'Olá, treinaweb!'
 
+let logradouro: string = "Avenida Paulista"
+let numero: string = "123b"
+let cidade: string = "São Paulo"
+let estado: string = "SP"
 
-console.log(criarMensagem('Lucas'))
+alert(`Endereço: ${logradouro}, ${numero}. ${cidade}, ${estado} `) //string partnes ou interpolação de string. Isso já nao é exclusividade do TS pois já é nativo do JS
