34bd28d79160a3f918d36314fdd8964864187065
lucasmetron <lucasmetron@gmail.com>
2021-09-01 16:11:27 -0300

    desconstruindo objetos parte 1

--- src/5 objetos spread e parametros com valores padrao/ob.ts
+++ src/5 objetos spread e parametros com valores padrao/ob.ts
@@ -2,7 +2,13 @@ let meusNumeros: number[] = [1, 2, 3, 4, 5]
 
 let [primeiro, segundo, terceiro, quarto, quinto] = meusNumeros; // Essa desconstrução já é nativa do JS
 let [numero1] = meusNumeros; // Essa desconstrução já é nativa do JS
+let [primeiroNumero, , terceiroNumero] = meusNumeros; // Essa desconstrução já é nativa do JS
+let [, , , , ultimo] = meusNumeros; // Essa desconstrução já é nativa do JS
+let [primeiroNumeroAgain, ...resto] = meusNumeros; // Essa desconstrução já é nativa do JS
 
 console.log(primeiro)
 console.log(numero1)
+console.log(primeiroNumero, terceiroNumero)
+console.log(ultimo)
 console.log(meusNumeros[4])
+console.log(resto)
