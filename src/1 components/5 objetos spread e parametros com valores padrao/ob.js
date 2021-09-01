5249fbc1de0ce507a000cdfd79b66e4f5c21b965
lucasmetron <lucasmetron@gmail.com>
2021-09-01 16:11:28 -0300

    desconstruindo objetos parte 1

--- src/1 components/5 objetos spread e parametros com valores padrao/ob.js
+++ src/1 components/5 objetos spread e parametros com valores padrao/ob.js
@@ -1,7 +1,13 @@
 "use strict";
 var meusNumeros = [1, 2, 3, 4, 5];
 var primeiro = meusNumeros[0], segundo = meusNumeros[1], terceiro = meusNumeros[2], quarto = meusNumeros[3], quinto = meusNumeros[4]; // Essa desconstrução já é nativa do JS
-var numero1 = meusNumeros[0];
-console.log(primeiro);
-console.log(numero1);
-console.log(meusNumeros[4]);
+var numero1 = meusNumeros[0]; // Essa desconstrução já é nativa do JS
+var primeiroNumero = meusNumeros[0], terceiroNumero = meusNumeros[2]; // Essa desconstrução já é nativa do JS
+var ultimo = meusNumeros[4]; // Essa desconstrução já é nativa do JS
+var primeiroNumeroAgain = meusNumeros[0], resto = meusNumeros.slice(1);
+// console.log(primeiro)
+// console.log(numero1)
+// console.log(primeiroNumero, terceiroNumero)
+// console.log(ultimo)
+// console.log(meusNumeros[4])
+console.log(resto);
